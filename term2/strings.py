412d509d1ea79c8e27a0b2a0e3e536e7233bd353
Seb Pound <sebpound@icloud.com>
2026-01-10 18:04:48 +0000

    strings

--- term2/strings.py
+++ term2/strings.py
@@ -1,2 +1,16 @@
-# Strings Example
-# Showing seb commits
+# surname example
+# inputs: Firstname, Lastname
+# outputs: first initial of Firstname Lastname all in uppercase
+
+# Get user inputs
+firstname = input("Enter Firstname: ")
+lastname = input("Enter Lastname: ")
+
+# Get the first initial and make it uppercase
+initial = firstname[0].upper()
+
+# Combine initial with lastname in uppercase
+result = initial + " " + lastname.upper()
+
+# Output the result
+print(result)
